02596be5057e8f43877160b79c3f661f52e7c592
Ra88-LBB <yl601225052@gmail.com>
2019-03-16 22:46:31 +0800

    Test1.0
    跟着书编了点

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,24 @@
-import sys
 import pygame
+from settings import Settings
+from ship import Ship
+import game_function as gf
+from pygame.sprite import Group
+
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT():
-                sys.exit()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets, aliens)
+        gf.update_bullets(bullets)
 
-        pygame.display.flip()
 
 run_game()
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1350
+        self.screen_height = 850
+        self.bg_color = (10, 10, 10)
+        self.ship_speed_factor = 3
+
+        self.bullet_speed_factor = 10
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (180, 180, 180)
